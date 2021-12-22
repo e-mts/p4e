8edae7f76cb9fa9a3a839707fafd5af568ef50ba
e-mts <61860605+e-mts@users.noreply.github.com>
2021-12-22 14:30:15 -0800

    Initialized

---
+++ ex_03_02.py
@@ -0,0 +1,12 @@
+while True:
+    try: 
+        timeHour = float(input("Enter your hours worked: "))
+        timeRate = float(input("Enter your hourly rate: "))
+        break
+    except:
+        print("\nNot a valid input! Try again.\n")
+if timeHour > 40.0:
+    timeOvertime = timeHour - 40.0
+    print('Your pay is: ${:.2f}.'.format(timeRate * (1.5 * timeOvertime + 40.0)))
+else:
+    print('Your pay is: ${:.2f}.'.format(timeHour * timeRate))
