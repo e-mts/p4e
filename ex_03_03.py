539cb7818c6893414f0504f40ac1fe72399d2fa6
e-mts <61860605+e-mts@users.noreply.github.com>
2021-12-22 19:52:15 -0800

    Finished ex_03_03

---
+++ ex_03_03.py
@@ -0,0 +1,21 @@
+from numpy import linspace
+
+while True:
+    try:
+        studentGrade = float(input('Enter a grade between 0 and 1: '))
+        if studentGrade<0 or studentGrade>1:
+            raise
+        break
+    except:
+        print('\nNot a valid input! Try again.\n')
+
+for i, j in enumerate(linspace(0.6, 1, 5)):
+    if j<= studentGrade:
+        continue
+    else:
+        break
+    
+if i == 0:
+    print("Your grade is: {}.".format(chr(70)))
+else:
+    print("Your grade is: {}.".format(chr(69 - i)))
